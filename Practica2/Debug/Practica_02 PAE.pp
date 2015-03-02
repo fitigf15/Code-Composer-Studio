66b77e098c48bd36ce1d418b0199d420cd62944e
Víctor Gómez <fiti.gf15@gmail.com>
2015-03-02 05:00:43 +0100

    Added practica 2

---
+++ Practica2/Debug/Practica_02 PAE.pp
@@ -0,0 +1,21 @@
+# FIXED
+
+Practica_02\ PAE.obj: ../Practica_02\ PAE.c
+Practica_02\ PAE.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430x54xA.h
+Practica_02\ PAE.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+Practica_02\ PAE.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics.h
+Practica_02\ PAE.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics_legacy_undefs.h
+Practica_02\ PAE.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/stdio.h
+Practica_02\ PAE.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/linkage.h
+Practica_02\ PAE.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/stdarg.h
+Practica_02\ PAE.obj: ../hal_lcd.h
+
+../Practica_02\ PAE.c: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430x54xA.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics_legacy_undefs.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/stdarg.h: 
+../hal_lcd.h: 
